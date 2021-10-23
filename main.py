5bec2a719264809d4d8a3d105454e99c476e389f
Vasilevsich <vgother@yandex.ru>
2021-10-23 11:26:12 +0300

    Hash-Function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import sha
 import mathRelated
+import hash_function
 
-print(sha.sha_256('hello world'))
-print(mathRelated.factorize_number(1000000))
-#print(mathRelated.calculate_primitive_root(1000))
+# print(sha.sha_256('hello world'))
+# print(mathRelated.factorize_number(1000000))
+# print(mathRelated.calculate_primitive_root(1000))
+print(hash_function.hash_function('q'))
+for i in  range(1, 101):
+    print(str(i))
+    hash_function.hash_function(str(i))
+
+"""for i in 'abcdefghijklmnopqrstuvwxyz':
+    print(i)
+    hash_function.hash_function(i)"""
